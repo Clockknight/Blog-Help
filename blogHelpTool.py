b8bbfea4d11339648631b32c07cd1b3eb9f12c73
Clockknight <Tylerf2wang@gmail.com>
2019-12-25 09:15:09 -0800

    Renamed blogHelp.py, and made it write to txt

--- blogHelpTool.py
+++ blogHelpTool.py
@@ -12,6 +12,12 @@ if sys.argv[1][-5:] != '.xlsx' or len(sys.argv) != 2:
 #os.exists check
 
 #Create variables based on spreadsheet
+book = openpyxl.load_workbook(str(sys.argv[1]))
+sheet = book.active
+    #Height variable
+height = sheet.max_row
+print(str(height - 1) + ' items found. Processing...')
+
 
 #Store data on content variable
 content += '''[su_table class="custom-su-table" responsive="yes"]
@@ -22,23 +28,17 @@ content += '''[su_table class="custom-su-table" responsive="yes"]
 <td>Player(s)</td>
 <td>Playtime</td>
 <td>Rating</td>
-</tr>
-'''
+</tr>'''
 
 for i in range(1, height):
     #Take href value from column 1, add it to an a tag with column 2's info
-    #nameString = ^^^
+    nameString = '^^^'
 
     #Write values from each column
-    content += '''
-
-    <tr>
-    <td>''' + sheet.cell(row=i, column=1).value + '''</td>
-    <td>''' + nameString + '''</td>
-    <td>''' + sheet.cell(row=i, column=3).value + '''</td>
-    <td>''' + sheet.cell(row=i, column=4).value + '''</td>
-    <td>''' + sheet.cell(row=i, column=5).value + '''</td>
-    </tr>'''
+    item = '\n\n<tr>\n<td>'
+    print(sheet.cell(row=i, column=1).value)
+    #item += sheet.cell(row=i, column=1).value + '</td>\n<td>' + nameString + '</td>\n<td>' + sheet.cell(row=i, column=3).value + '</td>\n<td>' + sheet.cell(row=i, column=4).value + '</td>\n<td>' + sheet.cell(row=i, column=5).value + '</td>\n</tr>'
+    content += item
 #End the su_table tag
 content += '\n[/su_table]'
 
