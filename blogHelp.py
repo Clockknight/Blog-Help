ab105d49a9184a8bf2caff57e9697573f53ca1af
Clockknight <Tylerf2wang@gmail.com>
2019-12-24 18:14:46 -0800

    Started writing to the txt file

--- blogHelp.py
+++ blogHelp.py
@@ -14,5 +14,37 @@ if sys.argv[1][-5:] != '.xlsx' or len(sys.argv) != 2:
 #Create variables based on spreadsheet
 
 #Store data on content variable
+content += '''[su_table class="custom-su-table" responsive="yes"]
+<table>
+<tr class="header">
+<td>Box Art</td>
+<td>Game</td>
+<td>Player(s)</td>
+<td>Playtime</td>
+<td>Rating</td>
+</tr>
+'''
+
+for i in range(1, height):
+    #Take href value from column 1, add it to an a tag with column 2's info
+    #nameString = ^^^
+
+    #Write values from each column
+    content += '''
+
+    <tr>
+    <td>''' + sheet.cell(row=i, column=1).value + '''</td>
+    <td>''' + nameString + '''</td>
+    <td>''' + sheet.cell(row=i, column=3).value + '''</td>
+    <td>''' + sheet.cell(row=i, column=4).value + '''</td>
+    <td>''' + sheet.cell(row=i, column=5).value + '''</td>
+    </tr>'''
+#End the su_table tag
+content += '\n[/su_table]'
+
+#Write out value
+file = open('blogHelp.txt', 'w+')
+file.write(content)
+file.close()
 
 #Save
