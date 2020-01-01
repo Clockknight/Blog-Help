598fb6aec308149bbfb1a962609f02b76536ec3a
Clockknight <Tylerf2wang@gmail.com>
2019-12-31 17:11:48 -0800

    Beginning of "non specific input" implementation

--- blogHelpTool.py
+++ blogHelpTool.py
@@ -1,3 +1,4 @@
+#Import libraries
 import os
 import sys
 import bs4
@@ -5,26 +6,57 @@ from bs4 import BeautifulSoup
 import openpyxl
 from openpyxl import workbook
 
+#Initialize variables
+filename = ''
+linkHeaders = ''
+
+argumentCount = len(sys.argv)
+
+artOperant = True
+gameOperant = True
+playerOperant = True
+timeOperant = True
+ratingOperant = True
+
+endValid = False
+columnValid = False
+
 #.xlsx argv check
-if sys.argv[1][-5:] != '.xlsx' or len(sys.argv) != 2:
-    print("Apologies. The argument provided did not lead to a valid .xlsx file. Please try again.")
-    sys.exit()
-#os.exists check
+if argumentCount > 1:
+    for n in (1, argumentCount - 1):
+        if str(sys.argv[n])[-5:] == '.xlsx':
+            filename = sys.argv[n]
+        if str(sys.argv[n]) == '-a':
+            artOperant = False
+        if str(sys.argv[n]) == '-g':
+            gameOperant = False
+        if str(sys.argv[n]) == '-p':
+            playerOperant = False
+        if str(sys.argv[n]) == '-t':
+            timeOperant = False
+        if str(sys.argv[n]) == '-r':
+            ratingOperant = False
+
+#validity checks + loops
 
 
 #Store data on spreadsheetText variable
-spreadsheetText = '''[su_table class="custom-su-table" responsive="yes"]
-<table>
-<tr class="header">
-<td>Box Art</td>
-<td>Game</td>
-<td>Player(s)</td>
-<td>Playtime</td>
-<td>Rating</td>
-</tr>'''
-linkHeaders = ''
+spreadsheetText = '[su_table class="custom-su-table" responsive="yes"]\n<table>\n<tr class="header'
+
+if artOperant:
+    spreadsheetText += '\n<td>Box Art</td>'
+if gameOperant:
+    spreadsheetText += '\n<td>Game</td>'
+if playerOperant:
+    spreadsheetText += '\n<td>Player(s)</td>'
+if timeOperant:
+    spreadsheetText += '\n<td>Playtime</td>'
+if ratingOperant :
+    spreadsheetText += '\n<td>Rating</td>'
+spreadsheetText += '\n</tr>'
+
 #Create variables based on spreadsheet
-book = openpyxl.load_workbook(str(sys.argv[1]))
+book = openpyxl.load_workbook(filename)
 sheet = book.active
     #Height variable
 height = sheet.max_row
@@ -53,7 +85,7 @@ for i in range(2, height+1):
 
         if j == 2:  #Special clause for 2nd column's data
             cellInfo = '\n<td>' + gameaTag + '</td>'
-            linkHeaders += '<h3>' + gameaTag  + '</h3>' + '\n[su_button url="' + amazonURL + '" target="blank" size="4" style="default" background="#EEC562" color="#000000" class="check-amazon"]Check availability on Amazon![/su_button]' + '\n<div style="height:100px" aria-hidden="true" class="wp-block-spacer">\n</div>\n\n'
+            linkHeaders += '<h2>' + gameaTag  + '</h2>' + '\n[su_button url="' + amazonURL + '" target="blank" size="4" style="default" background="#EEC562" color="#000000" class="check-amazon"]Check availability on Amazon![/su_button]' + '\n<div style="height:100px" aria-hidden="true" class="wp-block-spacer">\n</div>\n\n'
 
         #Write completed cellInfo to itemDetails
         itemDetails += cellInfo
@@ -64,6 +96,6 @@ spreadsheetText += '\n</table>\n[/su_table]\n\n'
 spreadsheetText += linkHeaders
 
 #Write out value
-file = open('blogHelp.txt', 'w+')
+file = open('htmlOutput.txt', 'w+')
 file.write(spreadsheetText)
 file.close()
